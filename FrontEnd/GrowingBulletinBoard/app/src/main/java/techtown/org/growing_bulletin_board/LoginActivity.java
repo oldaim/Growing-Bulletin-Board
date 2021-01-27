13df0bfb565aace5f376ab9407a0bd1d97174ea0
cheewr85 <32586985+cheewr85@users.noreply.github.com>
2021-01-27 17:15:12 +0900

    로그인 화면 구현
    간단하게 네이버/구글 로그인 버튼 구현함

---
+++ FrontEnd/GrowingBulletinBoard/app/src/main/java/techtown/org/growing_bulletin_board/LoginActivity.java
@@ -0,0 +1,14 @@
+package techtown.org.growing_bulletin_board;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
